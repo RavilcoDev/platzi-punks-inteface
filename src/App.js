15001ba80d4703fe5505ae468e61b1cda53d5282
Rafael Livise <r.livise@hotmail.com>
2022-06-23 09:59:12 -0500

    feat: [ADD] connection web3 wallet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+import { Home } from './views/Home'
+import MainLayout from './layouts/main'
+
+function App () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <MainLayout>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </MainLayout>
+    </Fragment>
   );
 }
 
